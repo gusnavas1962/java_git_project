ed28dfb4e58f620f80f0e82ebec4994cbfd2e2d0
Ubuntu <ubuntu@ip-172-31-34-255.us-east-2.compute.internal>
2023-12-04 21:19:05 +0000

    Segunda versión de las clases

--- src/com/app/PruebaAudioVisual.java
+++ src/com/app/PruebaAudioVisual.java
@@ -1,13 +1,23 @@
 package com.app;
 
 public class PruebaAudioVisual {
-	public static void main(String[] args) {
-	    	System.out.println("Hello from AWS Cloud9!");
-    		ContenidoAudiovisual av = new ContenidoAudiovisual("Avatar", 125, "Accion");
-    		ContenidoAudiovisual deeter = new ContenidoAudiovisual("De aqui a la Eternindad", 90, "accion");
-    		av.titulo = "De aqui a la Eternidad";
-    		av.duracionEnMinutos = 140;
-    		av.genero = "Guerra";
-	}
-	
+    public static void main(String[] args) {
+        System.out.println("Hello from AWS Cloud9!");
+
+        // Crear instancias adicionales de ContenidoAudiovisual
+        ContenidoAudiovisual av = new ContenidoAudiovisual("Avatar", 125, "Accion");
+        ContenidoAudiovisual deeter = new ContenidoAudiovisual("De aqui a la Eternindad", 90, "accion");
+        ContenidoAudiovisual inception = new ContenidoAudiovisual("Inception", 148, "Sci-Fi");
+        ContenidoAudiovisual starWars = new ContenidoAudiovisual("Star Wars", 135, "Fantasy");
+        ContenidoAudiovisual matrix = new ContenidoAudiovisual("Matrix", 136, "Sci-Fi");
+
+        // Mostrar los detalles de cada contenido audiovisual
+        System.out.println("Detalles del contenido:");
+        System.out.println("ID: " + matrix.getId());
+        System.out.println("Título: " + starWars.getTitulo());
+        System.out.println("Duración en minutos: " + starWars.getDuracionEnMinutos());
+        System.out.println("Género: " + inception.getGenero());
+        System.out.println();
+    }
+
 }
